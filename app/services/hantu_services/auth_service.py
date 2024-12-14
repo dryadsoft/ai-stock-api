8868e0c711bf18fcdf35828164444a4d79189a3e
dryadsoft <dryadsoft@gmail.com>
2024-12-14 12:30:52 +0900

    access token발급시 유효성처리 추가

--- app/services/hantu_services/auth_service.py
+++ app/services/hantu_services/auth_service.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests as rq
 from app.services.hantu_services.domain import Domain
 
@@ -7,6 +8,7 @@ class AuthService:
         self.BASE_URL = Domain.get_url()
         self.APP_KEY = Domain.get_app_key()
         self.SECRET_KEY = Domain.get_secret_key()
+        self.access_token_obj = None
 
     def get_approval_key(self):
         """실시간 접속키 발급"""
@@ -27,7 +29,6 @@ class AuthService:
                 f"실시간 접속키 발급에 실패하였습니다.[{response.status_code}]\n error_message: {response.text}"
             )
         else:
-            print(response)
             json = response.json()
             return json.get("approval_key")
 
@@ -50,33 +51,58 @@ class AuthService:
                 f"Hash 발급에 실패하였습니다.[{response.status_code}]\n error_message: {response.text}"
             )
         else:
-            print(response)
-            # json = response.json()
             return response.json()
+
+    def _valid_access_token(self):
+        """access token 유효성 검사"""
+        access_token = ""
+        if self.access_token_obj != None:
+            access_token_token_expired = self.access_token_obj[
+                "access_token_token_expired"
+            ]
+            now = datetime.now().date()
+            date_format = "%Y-%m-%d %H:%M:%S"
+            expired_date = datetime.strptime(
+                access_token_token_expired, date_format
+            ).date()
+            if expired_date > now:
+                print("access token이 유효합니다.")
+                access_token = self.access_token_obj["access_token"]
+            else:
+                print("access token이 유효하지 않습니다. 폐기후 재발급요청 하겠습니다.")
+                self.revoke_access_token(self.access_token_obj["access_token"])
+                self.access_token_obj = None
+        return access_token
 
     def get_access_token(self):
         """access token 발급"""
-        headers = {
-            "content-type": "application/json; utf-8",
-        }
-        json_body = {
-            "grant_type": "client_credentials",
-            "appkey": self.APP_KEY,
-            "appsecret": self.SECRET_KEY,
-        }
-        response = rq.post(
-            f"{self.BASE_URL}/oauth2/tokenP",
-            headers=headers,
-            json=json_body,
-        )
-        if response.status_code != 200:
-
-            raise Exception(
-                f"access token 발급에 실패하였습니다.[{response.status_code}]\n error_message: {response.text}"
-            )
+        access_token = self._valid_access_token()
+        if access_token != "":
+            return access_token
         else:
-            print(response)
-            return response.json()
+            headers = {
+                "content-type": "application/json; utf-8",
+            }
+            json_body = {
+                "grant_type": "client_credentials",
+                "appkey": self.APP_KEY,
+                "appsecret": self.SECRET_KEY,
+            }
+            response = rq.post(
+                f"{self.BASE_URL}/oauth2/tokenP",
+                headers=headers,
+                json=json_body,
+            )
+            if response.status_code != 200:
+
+                raise Exception(
+                    f"access token 발급에 실패하였습니다.[{response.status_code}]\n error_message: {response.text}"
+                )
+            else:
+                print("access token이 발급되었습니다.")
+                self.access_token_obj = response.json()
+                access_token = self._valid_access_token()
+                return access_token
 
     def revoke_access_token(self, access_token):
         """access token 폐기"""
