1fa6dd71efaa7b93d31a209239f1241b2141cb09
dryadsoft <dryadsoft@gmail.com>
2024-12-15 15:30:02 +0900

    _get_properties 명칭변경, get_is_prod 추가

--- app/services/hantu_services/domain.py
+++ app/services/hantu_services/domain.py
@@ -6,22 +6,26 @@ class Domain(Enum):
     IS_PROD = config_yaml.get_propertity("is_prod_hantu")
 
     @classmethod
-    def _get_env(self):
-        if self.IS_PROD.value:
+    def _get_properties(self):
+        if self.get_is_prod():
             return config_yaml.get_propertity("hantu_prod")
         return config_yaml.get_propertity("hantu_dev")
 
     @classmethod
+    def get_is_prod(self):
+        return self.IS_PROD.value
+
+    @classmethod
     def get_url(self):
-        hantu = self._get_env()
+        hantu = self._get_properties()
         return hantu["api_base_url"]
 
     @classmethod
     def get_cano(self):
-        hantu = self._get_env()
+        hantu = self._get_properties()
         return hantu["cano"]
 
     @classmethod
     def get_acnt_prdt_cd(self):
-        hantu = self._get_env()
+        hantu = self._get_properties()
         return hantu["acnt_prdt_cd"]
